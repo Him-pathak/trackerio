c81e060dad61c0d002530dda2e859a58705a287f
Him-pathak <pathakhimanshukr@gmail.com>
2023-10-26 18:41:59 +0530

    static left panel

---
+++ src/Components/Middle/Heading.jsx
@@ -0,0 +1,12 @@
+import { Stack, Typography } from '@mui/material'
+import React from 'react'
+
+function Heading() {
+  return (
+    <Stack>
+        <Typography>Habit 1</Typography>
+    </Stack>
+  )
+}
+
+export default Heading
--- src/Sections/Right.jsx
+++ src/Sections/Right.jsx
@@ -1,10 +1,12 @@
+import { Stack } from '@mui/material'
 import React from 'react'
+import Heading from '../Components/Middle/Heading'
 
 function Right() {
   return (
-    <div>
-      Right
-    </div>
+    <Stack sx={{padding:'15px'}}>
+        <Heading/>
+    </Stack>
   )
 }
 
